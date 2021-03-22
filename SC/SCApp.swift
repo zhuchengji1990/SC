d82c033956d323f8127aca1bfe7f2bd28f3957e4
chenji <83136645@qq.com>
2021-03-22 19:42:24 +0800

    Initial Commit

---
+++ SC/SCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCApp.swift
+//  SC
+//
+//  Created by 沉寂 on 2021/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
